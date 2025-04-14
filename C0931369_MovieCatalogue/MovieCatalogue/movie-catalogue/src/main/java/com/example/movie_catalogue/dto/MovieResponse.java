90244f3035b79831669a0738811f93044d86b1fb
Mahedi004 <c0931369@mylambton.ca>
2025-04-13 23:23:39 -0400

    Delete C0931369_MovieCatalogue/MovieCatalogue/movie-catalogue directory

--- C0931369_MovieCatalogue/MovieCatalogue/movie-catalogue/src/main/java/com/example/movie_catalogue/dto/MovieResponse.java
+++
@@ -1,8 +0,0 @@
-package com.example.movie_catalogue.dto;
-
-public class MovieResponse {
-    private MovieDTO[] results;
-
-    public MovieDTO[] getResults() { return results; }
-    public void setResults(MovieDTO[] results) { this.results = results; }
-}
--- C0931369_MovieCatalogue/MovieCatalogue/movie-catalogue/src/main/java/com/example/movie_catalogue/repository/MovieRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.movie_catalogue.repository;
-
-import com.example.movie_catalogue.model.Movie;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MovieRepository extends JpaRepository<Movie, Long> {
-}
